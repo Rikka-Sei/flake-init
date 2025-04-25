3c5ec6e6287e59bb0eae1c8b8a8414f302602782
Rikki <rikki@member.fsf.org>
2025-04-25 09:25:05 +0800

    feat: NixOS 支持

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "flake-init: a bash project template with flake-utils";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bash
+            coreutils
+            findutils
+            gnugrep
+            gnused
+          ];
+        };
+
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "flake-init";
+          version = "0.1.0";
+
+          src = ./src;
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp flake-init.sh $out/bin/flake-init
+            chmod +x $out/bin/flake-init
+          '';
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/flake-init";
+        };
+      }
+    );
+}
