4d6f5c29334cd5c18b590b86505a1689f59bafd5
Rikki <rikki@member.fsf.org>
2025-04-26 01:50:42 +0800

    fix: 修复 cancelThenExit 函数不能正确捕捉传出参数的问题

--- src/utils.sh
+++ src/utils.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-echo 1
-
 cancelThenExit(){
+    local return_code=$?
     # 如果没有传入参数，默认使用1作为退出码
     local exit_code=${1:-1}  
 
-    if [ $? -ne 0 ]; then 
+    if [ $return_code -ne 0 ]; then 
         exit $exit_code;
     fi
 }
