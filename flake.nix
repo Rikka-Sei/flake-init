afd03ae77b5863130d743c1e3fe7552bbcabbc60
Rikki <rikki@member.fsf.org>
2025-04-25 09:34:03 +0800

    feat: 增加 #flake-init 程序定位符,格式化flake文件

--- flake.nix
+++ flake.nix
@@ -6,11 +6,19 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             bash
@@ -34,9 +42,16 @@
           '';
         };
 
-        apps.default = {
-          type = "app";
-          program = "${self.packages.${system}.default}/bin/flake-init";
+        apps = {
+          default = {
+            type = "app";
+            program = "${self.packages.${system}.default}/bin/flake-init";
+          };
+
+          flake-init = {
+            type = "app";
+            program = "${self.packages.${system}.default}/bin/flake-init";
+          };
         };
       }
     );
